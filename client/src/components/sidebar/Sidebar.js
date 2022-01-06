e1559dde335e4c5a43165623c1db78b60978ec7a
arisa <angelevarela@hotmail.com>
2022-01-05 20:50:07 -0800

    Left sidebar 50% complete

--- client/src/components/sidebar/Sidebar.js
+++ client/src/components/sidebar/Sidebar.js
@@ -10,53 +10,75 @@ import Register from '../auth/Register'
 
 import './Sidebar.scss'
 
-
+import {BsTwitter, BsBell, BsEnvelopeOpen, BsPersonFill} from 'react-icons/bs'
+import {FaHome} from 'react-icons/fa'
+import {HiOutlineHashtag} from 'react-icons/hi'
 
 function Sidebar(){
-
-    // TODO : PLACE THE USER OBJECT IN THE USER CONTEXT SO WE CAN NARROW IT
+    const navigate = useNavigate()    
     const {user, getUser} = useContext(UserContext)
-
-    const navigate = useNavigate()
 
     async function logout(){
         await Axios.get(`${domain}/auth/logout`)
         await getUser()
         navigate('/')
     }
-
     
     return (
-        <div className='sidebar'>
-            <div className='displayList'>
-                <p>Icon</p>
-
-
-
-                <p>
-                    <Link to='/' className='home-link'>Home</Link>
-                </p>
+        <div className='entire-sidebar'>
+            
+            <div className='sidebar-icons'>
+                <div className="sidebar-bird">
+                    <BsTwitter/>
+                </div>
                 
+                <div className="sidebar-home">
+                    <p>
+                        <Link to='/' className='home-link'><FaHome/><span>Home</span></Link> 
+                    </p>
+                </div>
+
+                <div className="sidebar-explore">
+                    <p>
+                        <HiOutlineHashtag/> <span>Explore</span>
+                    </p>
+                </div>
+
+                <div className="sidebar-notifications">
+                    <p>
+                        <BsBell/> <span>Notifications</span>
+                    </p>
+                </div>
                 
-                <p>Explore</p>
-                <p>Notifications</p>
-                <p>Messages</p>
+                <div className="sidebar-messages">
+                    <p>
+                        <BsEnvelopeOpen/><span>Messages</span>
+                    </p>
+                </div>
                 
                 <p>
                     {user === null || user.username === null ? (
                         // <Link to={`/profile/`} className='profile-link'>Profile</Link>
-                        <Link to='register' className='register-an-account'>Profile</Link>
+                        <Link to='register' className='profile-link'><BsPersonFill/><span>Profile</span></Link>
                     ): (
                         <Link to={`/profile/${user.username}`} className='profile-link'>Profile</Link>
                     )}
                     
                 </p>
 
+                <div className="sidebar-tweet-div">
+                    <p>
+                        <button className='sidebar-tweet-btn'>
+                            <span>
+                                Tweet    
+                            </span>
+                        </button>
+                    </p>
+                </div>
             </div>
             
             
-            <div className='authList'>
-                
+            <div className='sidebar-authlist'>
                 {user === null || user.username === null? (
                     <p>
                         <Link to='register' className='register'>Register</Link>
@@ -70,6 +92,7 @@ function Sidebar(){
                         </button>
                 )}
             </div>
+
         </div>
     )
 }
